536318cc78000e85c949cae2612d1641333282ad
CrisjohnOfficial <crisjohnramirez521@gmail.com>
2024-01-19 03:33:24 -1200

    adding files and Img for Dice

---
+++ script.js
@@ -0,0 +1,21 @@
+const { values } = require("lodash");
+
+
+function rollDice() {
+
+
+    const numOfDice = document.getElementById('numOfDice').value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImg = document.getElementById('diceImg');
+    const values = [];
+    const images = [];
+
+    for(let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="assets/images/${value}.png" >`);
+    }
+
+    diceResult.textContent = `Dice: ${values.join(", ")}`;
+    diceImg.innerHTML = images.join('');
+};
